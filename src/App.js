fbc31be0ddf6a4fafa5595c2075dfbcb265e5c9a
Antoin Campbell <antoinfive@gmail.com>
2017-10-09 17:01:19 -0400

    beginning to add components, actions, and basic styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ import Flexbox from 'flexbox-react';
 import Portfolio from './components/portfolio';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      portfolio: false,
+      blog: false,
+      mind: false,
+      me: true
+    }
+
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+    handleOnClick(attr) {
+
+      this.setState({
+        [attr]: true
+      })
+
+    }
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
@@ -19,25 +40,23 @@ class App extends Component {
       <div className="App-navLinks">
           <Flexbox flexDirection="row" minWidth="10vh">
             <Flexbox flexGrow={1}>
-              <h3 className="navLinksText" onClick={()=> { alert('hi') }}> portfolio </h3>
+              <h3 className="navLinksText" onClick={this.handleOnClick.bind(null, "portfolio")}> portfolio </h3>
             </Flexbox>
 
             <Flexbox flexGrow={1}>
-              <h3 className="navLinksText"> blog </h3>
+              <h3 className="navLinksText" onClick={this.handleOnClick.bind(null, "blog")}> blog </h3>
             </Flexbox>
 
             <Flexbox flexGrow={1}>
-               <h3 className="navLinksText"> contact </h3>
+               <h3 className="navLinksText" onClick={this.handleOnClick.bind(null, "contact")}> contact </h3>
             </Flexbox>
 
             <Flexbox flexGrow={1}>
-               <h3 className="navLinksText"> mind </h3>
+               <h3 className="navLinksText" onClick={this.handleOnClick.bind(null, "mind")}> mind </h3>
             </Flexbox>
           </Flexbox>
         </div>
-
-
-        <div> <Portfolio/> </div>
+        
       </div>
     );
   }
